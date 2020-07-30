f7308dcc4b55b6f785310a25f35661ae4bcec6fe
Joseph Morag <jm4157@columbia.edu>
2020-07-30 12:50:51 -0400

    Create FingeringEditor class to be able to retrieve modified MusicXml

--- static/js/fingering_editor.js
+++ static/js/fingering_editor.js
@@ -1,149 +1,177 @@
-function get_svg_fingerings(svg) {
-  let svg_fingerings = [];
-  svg
-    .querySelectorAll("g.vf-stavenote>g.vf-modifiers")
-    .forEach(m =>
-      m.querySelectorAll("text").forEach(f => svg_fingerings.push(f))
-    );
-  svg_fingerings.forEach(
-    f => f.textContent === "-1" && f.setAttribute("visibility", "hidden")
-  );
-  return svg_fingerings;
-}
+class FingeringEditor {
+  constructor(nodeId) {
+    this.nodeId = nodeId;
+    this.xml = null;
+    this.index = 0;
+    this.svg_fingerings = null;
+    this.svg_noteheads = null;
+    this.unfocused = "#000000";
+    this.focused = "#34d8eb";
+    this.handleKeypress = this.handleKeypress.bind(this);
+  }
 
-function get_svg_noteheads(svg) {
-  let svg_noteheads = [];
-  svg.querySelectorAll("g.vf-stavenote").forEach(
-    n =>
-      // filter rests
-      n.querySelector("g.vf-modifiers").childElementCount > 0 &&
-      n
-        .querySelectorAll("g.vf-notehead>path")
-        .forEach(n => svg_noteheads.push(n))
-  );
-  return svg_noteheads;
-}
-
-async function render_musicxml(xml, nodeId) {
-  const osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay(nodeId, {
-    autoResize: true,
-    backend: "svg",
-    drawingParameters: "compact",
-    drawPartNames: false,
-    drawTitle: true,
-    drawFingerings: true,
-    fingeringPosition: "left",
-    setWantedStemDirectionByXml: true,
-    drawMeasureNumbers: false,
-    autoBeam: false,
-    pageFormat: "Endless"
-  });
-
-  xml.querySelectorAll("note").forEach(function(note) {
-    if (note.querySelector("rest")) {
-      return;
-    }
-    if (note.querySelector("notations") === null) {
-      let notations = xml.createElement("notations");
-      let technical = xml.createElement("technical");
-      let fingering = xml.createElement("fingering");
-      fingering.appendChild(xml.createTextNode("-1"));
-      technical.appendChild(fingering);
-      notations.appendChild(technical);
-      note.appendChild(notations);
-    } else if (note.querySelector("notations>technical") === null) {
-      let technical = xml.createElement("technical");
-      let fingering = xml.createElement("fingering");
-      fingering.appendChild(xml.createTextNode("-1"));
-      technical.appendChild(fingering);
-      note.querySelector("notations").appendChild(technical);
-    } else if (note.querySelector("notations>technical>fingering") === null) {
-      let fingering = xml.createElement("fingering");
-      fingering.appendChild(xml.createTextNode("-1"));
-      note.querySelector("notations>technical").appendChild(fingering);
-    }
-  });
-  await osmd.load(xml);
-  osmd.render;
-  const unfocused = "#000000";
-  const selected = "#34d8eb";
-  let target = document.getElementById(nodeId);
-
-  const observer = new MutationObserver(function(_) {
-    const svg = document.querySelector("svg");
-    const svg_noteheads = get_svg_noteheads(svg);
-    const svg_fingerings = get_svg_fingerings(svg);
-    console.log(svg_noteheads.length, svg_fingerings.length);
-
-    let index = 0;
-    svg_noteheads[index].setAttribute("fill", selected);
-
-    const next = function() {
-      if (index >= svg_noteheads.length - 1) return;
-      svg_noteheads[index].setAttribute("fill", unfocused);
-      index++;
-      svg_noteheads[index].setAttribute("fill", selected);
-    };
-
-    const prev = function() {
-      if (index <= 0) return;
-      svg_noteheads[index].setAttribute("fill", unfocused);
-      index--;
-      svg_noteheads[index].setAttribute("fill", selected);
-    };
-
-    const handleKeypress = function(e) {
-      console.log(e);
-      let finger = svg_fingerings[index];
-      const setFinger = n => {
-        finger.textContent = n;
-        xml.querySelectorAll("fingering")[index].textContent = n;
-        finger.removeAttribute("visibility");
-        next();
-      };
-      switch (e.key) {
-        case "ArrowRight":
-          next();
-          break;
-        case "ArrowLeft":
-          prev();
-          break;
-        case "`":
-          setFinger("0");
-          break;
-        case "0":
-          setFinger("0");
-          break;
-        case "1":
-          setFinger("1");
-          break;
-        case "2":
-          setFinger("2");
-          break;
-        case "3":
-          setFinger("3");
-          break;
-        case "4":
-          setFinger("4");
-          break;
-        case "Backspace":
-          finger.textContent = "-1";
-          finger.setAttribute("visibility", "hidden");
-          prev();
-          break;
-        case "Enter":
-          break;
+  get musicxml() {
+    const xml = this.xml.cloneNode(true)
+    xml.querySelectorAll("fingering").forEach(function(fingering) {
+      if (fingering.textContent === "-1") {
+        const technical = fingering.parentNode;
+        const notations = technical.parentNode;
+        const note = notations.parentNode;
+        technical.removeChild(fingering);
+        if (!technical.childElementCount) {
+          notations.removeChild(technical);
+          if (!notations.childElementCount) {
+            note.removeChild(notations);
+          }
+        }
       }
-    };
-    window.addEventListener("keydown", handleKeypress);
-  });
-  observer.observe(target, { childList: true });
-}
+    });
+    return xml;
+  }
 
-function clean_xml(xml) {
-  xml
-    .querySelectorAll("fingering")
-    .forEach(
-      node => node.textContent === "-1" && node.parentNode.removeChild(node)
+  static parse_xml(xml_string) {
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(xml_string, "application/xml");
+    xml.querySelectorAll("note").forEach(function(note) {
+      if (note.querySelector("rest")) {
+        return;
+      }
+      if (note.querySelector("notations") === null) {
+        let notations = xml.createElement("notations");
+        let technical = xml.createElement("technical");
+        let fingering = xml.createElement("fingering");
+        fingering.appendChild(xml.createTextNode("-1"));
+        technical.appendChild(fingering);
+        notations.appendChild(technical);
+        note.appendChild(notations);
+      } else if (note.querySelector("notations>technical") === null) {
+        let technical = xml.createElement("technical");
+        let fingering = xml.createElement("fingering");
+        fingering.appendChild(xml.createTextNode("-1"));
+        technical.appendChild(fingering);
+        note.querySelector("notations").appendChild(technical);
+      } else if (note.querySelector("notations>technical>fingering") === null) {
+        let fingering = xml.createElement("fingering");
+        fingering.appendChild(xml.createTextNode("-1"));
+        note.querySelector("notations>technical").appendChild(fingering);
+      }
+    });
+    return xml;
+  }
+
+  set_fingerings(svg) {
+    let svg_fingerings = [];
+    svg
+      .querySelectorAll("g.vf-stavenote>g.vf-modifiers")
+      .forEach(m =>
+        m.querySelectorAll("text").forEach(f => svg_fingerings.push(f))
+      );
+    svg_fingerings.forEach(
+      f => f.textContent === "-1" && f.setAttribute("visibility", "hidden")
     );
+    this.svg_fingerings = svg_fingerings;
+  }
+
+  set_noteheads(svg) {
+    let svg_noteheads = [];
+    svg.querySelectorAll("g.vf-stavenote").forEach(
+      n =>
+        // filter rests
+        n.querySelector("g.vf-modifiers").childElementCount > 0 &&
+        n
+          .querySelectorAll("g.vf-notehead>path")
+          .forEach(n => svg_noteheads.push(n))
+    );
+    svg_noteheads[0].setAttribute("fill", this.focused);
+    this.svg_noteheads = svg_noteheads;
+  }
+
+  next() {
+    if (this.index >= this.svg_noteheads.length - 1) return;
+    this.svg_noteheads[this.index].setAttribute("fill", this.unfocused);
+    this.index++;
+    this.svg_noteheads[this.index].setAttribute("fill", this.focused);
+  }
+
+  prev() {
+    if (this.index <= 0) return;
+    this.svg_noteheads[this.index].setAttribute("fill", this.unfocused);
+    this.index--;
+    this.svg_noteheads[this.index].setAttribute("fill", this.focused);
+  }
+
+  setFinger(n) {
+    let finger = this.svg_fingerings[this.index];
+    finger.textContent = n;
+    this.xml.querySelectorAll("fingering")[this.index].textContent = n;
+    finger.removeAttribute("visibility");
+    this.next();
+  }
+
+  handleKeypress(e) {
+    console.log(e.key, this.index);
+    let finger = this.svg_fingerings[this.index];
+    switch (e.key) {
+      case "ArrowRight":
+        this.next();
+        break;
+      case "ArrowLeft":
+        this.prev();
+        break;
+      case "`":
+        this.setFinger("0");
+        break;
+      case "0":
+        this.setFinger("0");
+        break;
+      case "1":
+        this.setFinger("1");
+        break;
+      case "2":
+        this.setFinger("2");
+        break;
+      case "3":
+        this.setFinger("3");
+        break;
+      case "4":
+        this.setFinger("4");
+        break;
+      case "Backspace":
+        finger.textContent = "-1";
+        finger.setAttribute("visibility", "hidden");
+        this.prev();
+        break;
+      case "Enter":
+        break;
+    }
+  }
+
+  async render(xml_string) {
+    this.index = 0;
+    this.xml = this.constructor.parse_xml(xml_string);
+    window.removeEventListener("keydown", this.handleKeypress);
+    const target = document.getElementById(this.nodeId);
+    const osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay(this.nodeId, {
+      autoResize: true,
+      backend: "svg",
+      drawingParameters: "compact",
+      drawPartNames: false,
+      drawTitle: true,
+      drawFingerings: true,
+      fingeringPosition: "left",
+      setWantedStemDirectionByXml: true,
+      drawMeasureNumbers: false,
+      autoBeam: false,
+      pageFormat: "Endless"
+    });
+    await osmd.load(this.xml);
+    osmd.render();
+    const observer = new MutationObserver(_ => {
+      const svg = document.querySelector("svg");
+      this.set_noteheads(svg);
+      this.set_fingerings(svg);
+    });
+    observer.observe(target, { childList: true });
+    window.addEventListener("keydown", this.handleKeypress);
+  }
 }
