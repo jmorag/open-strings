cd1d6c8a4afe3d47af19075875becae3c728d175
Joseph Morag <jm4157@columbia.edu>
2020-08-14 14:17:08 -0400

    Add imslp handler

---
+++ src/Handler/IMSLP.hs
@@ -0,0 +1,65 @@
+module Handler.IMSLP where
+
+import Control.Lens hiding ((.=))
+import Control.Lens.Regex.Text
+import qualified Data.Text as T
+import Import
+import Network.HTTP.Simple
+import Text.HTML.DOM
+import Text.XML.Lens
+
+htmlOf :: (MonadUnliftIO m) => Request -> m Document
+htmlOf = fmap (parseLBS . getResponseBody) . httpLBS
+
+findMovements :: Document -> [Text]
+findMovements doc =
+  doc
+    ^.. root
+      . deep (el "tr")
+      . taking 1 (filtered (\tr -> Just "Movements/Sections" == tr ^? deep (el "span" . text)))
+      . failing (deep (el "li" . text)) (deep (el "dd" . text))
+      . to (dropKeys . dropRomanNumerals . T.strip . fixSpaces)
+  where
+    fixSpaces = T.map \case '\160' -> ' '; c -> c
+    dropRomanNumerals = T.dropWhile (`elem` ("IVX." :: [Char]))
+    dropKeys = set ([regex|\s\([A-G][^\s]*\s(minor|major)\)|] . match) ""
+
+findInstrumentation :: Document -> [Text]
+findInstrumentation doc =
+  maybe [] (T.splitOn ", ") $
+    doc
+      ^? root
+        . deep (el "tr")
+        . filtered (\tr -> tr ^? deep (el "th" . text) . to T.strip == Just "Instrumentation")
+        . deep (el "td" . text)
+        . to T.strip
+
+getIMSLPR :: (MonadThrow m, MonadUnliftIO m) => String -> m Value
+getIMSLPR imslp = do
+  request <- parseRequest imslp
+  doc <- htmlOf request
+  let imslp' = T.pack imslp
+      title =
+        decodeUrl
+          . T.strip
+          . T.map (\case '_' -> ' '; c -> c)
+          . T.takeWhileEnd (/= '/')
+          . T.dropEnd 1
+          . T.dropWhileEnd (/= '(')
+          $ imslp'
+      composer =
+        decodeUrl
+          . T.strip
+          . T.map (\case '_' -> ' '; c -> c)
+          . T.dropEnd 1
+          . T.takeWhileEnd (/= '(')
+          $ imslp'
+  pure $
+    object
+      [ "instrumentation" .= findInstrumentation doc,
+        "movements" .= findMovements doc,
+        "composer" .= composer,
+        "title" .= title
+      ]
+  where
+    decodeUrl = decodeUtf8 . urlDecode False . encodeUtf8
--- src/Model/Parts.hs
+++ src/Model/Parts.hs
@@ -1,9 +1,16 @@
+{-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Model.Parts where
 
 import ClassyPrelude.Yesod
-import Dhall
+import Control.Lens
+import Data.Aeson
+import Data.Aeson.Types
+import Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.Set as Set
+import Dhall (FromDhall, Natural)
+import Validation
 
 data Solo = Solo | Tutti
   deriving (Show, Eq, Read, Ord, Generic)
@@ -13,13 +20,79 @@ data StringInstrument = Violin | Viola | Cello | Bass
 
 data Part = Part
   { instrument :: StringInstrument,
+    -- | Parts default to solo unless tutti is indicated
     solo :: Solo,
     -- | Part 0 denotes that it's the only instrument in the score, i.e.
     -- violin concerto soloist is Part Violin Solo 0
     -- whereas the first violinist in a quartet is Part Violin Solo 1
     part_num :: Natural
   }
-  deriving (Show, Eq, Read, Ord, Generic)
+  deriving (Show, Eq, Read, Generic)
+
+-- We can't just switch the order of the fields in Part and use the derived instance
+-- because it breaks the database instance
+instance Ord Part where
+  compare p1 p2 =
+    compare (solo p1) (solo p2)
+      <> compare (instrument p1) (instrument p2)
+      <> compare (part_num p1) (part_num p2)
+
+instance ToJSON Part where
+  toJSON Part {..} =
+    String $
+      case solo of Solo -> ""; Tutti -> "Tutti "
+        <> tshow instrument
+        <> case part_num of 0 -> ""; _ -> " " <> tshow part_num
+
+instance {-# OVERLAPS #-} FromJSON (Set Part) where
+  parseJSON =
+    withArray "Set Part" $
+      fmap (Set.fromList . concat)
+        -- can't use withText here because it short circuits the validation applicative
+        . ( hoistValidation
+              . traverse
+                ( \case
+                    (String p) -> parsePart p
+                    invalid -> failure ("Expected String, got " <> show invalid)
+                )
+          )
+
+parsePart :: Text -> Validation (NonEmpty String) [Part]
+parsePart p = case fmap (over (ix 0) charToUpper) (words p) of
+  ["Quartet"] ->
+    pure
+      [ Part Violin Solo 1,
+        Part Violin Solo 2,
+        Part Viola Solo 0,
+        Part Cello Solo 0
+      ]
+  ["Strings"] ->
+    pure
+      [ Part Violin Tutti 1,
+        Part Violin Tutti 2,
+        Part Viola Tutti 0,
+        Part Cello Tutti 0
+      ]
+  ["Tutti", instrument] -> part Tutti instrument "0"
+  ["Tutti", instrument, num] -> part Tutti instrument num
+  ["Solo", instrument] -> part Solo instrument "0"
+  ["Solo", instrument, num] -> part Solo instrument num
+  [instrument] -> part Solo instrument "0"
+  [instrument, num] -> part Solo instrument num
+  _ -> failure ("Unable to parse part " <> unpack p)
+  where
+    part :: Solo -> Text -> Text -> Validation (NonEmpty String) [Part]
+    part solo instr num = do
+      i <- parseInstrument instr
+      n <- parsePartNum num
+      pure [Part i solo n]
+      where
+        validateRead fn_err x = maybeToSuccess (fn_err x :| []) (readMay x)
+        parsePartNum = validateRead (\n -> "Unknown part number " <> show n)
+        parseInstrument = validateRead (\i -> "Unknown instrument " <> unpack i)
+
+hoistValidation :: Validation (NonEmpty String) a -> Parser a
+hoistValidation = validation (parseFail . unlines) pure
 
 derivePersistField "Part"
 
