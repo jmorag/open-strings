a6d60490e461c43cbb541775027a9c3157b4dfff
Joseph Morag <jm4157@columbia.edu>
2020-08-24 18:28:58 -0400

    Comment out default specs

--- test/Handler/HomeSpec.hs
+++ test/Handler/HomeSpec.hs
@@ -1,35 +1,38 @@
-{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Handler.HomeSpec (spec) where
 
 import TestImport
 
 spec :: Spec
-spec = withApp $ do
+spec = withApp (pure ())
 
-    describe "Homepage" $ do
-        it "loads the index and checks it looks right" $ do
-          get HomeR
-          statusIs 200
-          htmlAnyContain "h1" "a modern framework for blazing fast websites"
+-- do
 
-          request $ do
-              setMethod "POST"
-              setUrl HomeR
-              addToken
-              fileByLabelExact "Choose a file" "test/Spec.hs" "text/plain" -- talk about self-reference
-              byLabelExact "What's on the file?" "Some Content"
+-- describe "Homepage" $ do
+--     it "loads the index and checks it looks right" $ do
+--       get HomeR
+--       statusIs 200
+--       htmlAnyContain "h1" "a modern framework for blazing fast websites"
 
-          statusIs 200
-          -- more debugging printBody
-          htmlAllContain ".upload-response" "text/plain"
-          htmlAllContain ".upload-response" "Some Content"
+--       request $ do
+--           setMethod "POST"
+--           setUrl HomeR
+--           addToken
+--           fileByLabelExact "Choose a file" "test/Spec.hs" "text/plain" -- talk about self-reference
+--           byLabelExact "What's on the file?" "Some Content"
 
-        -- This is a simple example of using a database access in a test.  The
-        -- test will succeed for a fresh scaffolded site with an empty database,
-        -- but will fail on an existing database with a non-empty user table.
-        it "leaves the user table empty" $ do
-          get HomeR
-          statusIs 200
-          users <- runDB $ selectList ([] :: [Filter User]) []
-          assertEq "user table empty" 0 $ length users
+--       statusIs 200
+--       -- more debugging printBody
+--       htmlAllContain ".upload-response" "text/plain"
+--       htmlAllContain ".upload-response" "Some Content"
+
+--     -- This is a simple example of using a database access in a test.  The
+--     -- test will succeed for a fresh scaffolded site with an empty database,
+--     -- but will fail on an existing database with a non-empty user table.
+--     it "leaves the user table empty" $ do
+--       get HomeR
+--       statusIs 200
+--       users <- runDB $ selectList ([] :: [Filter User]) []
+--       assertEq "user table empty" 0 $ length users
