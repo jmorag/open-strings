749688d0a84f7e8df48ca2e6c7dee6ef0cf15a71
Joseph Morag <jm@josephmorag.com>
2021-03-07 15:57:25 -0500

    Remove extra stuff from custom google oauth

--- src/Yesod/Auth/OAuth2/Google/Custom.hs
+++ src/Yesod/Auth/OAuth2/Google/Custom.hs
@@ -1,58 +1,16 @@
-module Yesod.Auth.OAuth2.Google.Custom (customOauth2GoogleScoped, oauth2GoogleScopedWidget) where
+module Yesod.Auth.OAuth2.Google.Custom (oauth2GoogleScopedWidget) where
 
 import Yesod.Auth
 import Yesod.Auth.OAuth2
-import Yesod.Auth.OAuth2.Dispatch
 import Yesod.Auth.OAuth2.Prelude
 import Yesod.Core.Widget
 import Prelude
 
+-- TODO: upstream this into yesod-auth-oauth2. It's almost entirely
+-- copy-pasted from their source
+
 pluginName :: Text
 pluginName = "google"
-
-customOauth2GoogleScoped ::
-  forall m.
-  YesodAuth m =>
-  [Text] ->
-  Text ->
-  Text ->
-  (Route m -> WidgetFor m ()) ->
-  AuthPlugin m
-customOauth2GoogleScoped scopes clientId clientSecret mkWidget =
-  AuthPlugin
-    pluginName
-    (dispatchAuthRequest pluginName oauth2 fetchAccessToken getCreds)
-    login
-  where
-    oauth2 =
-      OAuth2
-        { oauthClientId = clientId
-        , oauthClientSecret = Just clientSecret
-        , oauthOAuthorizeEndpoint =
-            "https://accounts.google.com/o/oauth2/auth"
-              `withQuery` [scopeParam " " scopes]
-        , oauthAccessTokenEndpoint =
-            "https://www.googleapis.com/oauth2/v3/token"
-        , oauthCallback = Nothing
-        }
-
-    login tm = mkWidget (tm (oauth2Url pluginName))
-
-getCreds :: FetchCreds m
-getCreds manager token = do
-  (User userId, userResponse) <-
-    authGetProfile
-      pluginName
-      manager
-      token
-      "https://www.googleapis.com/oauth2/v3/userinfo"
-
-  pure
-    Creds
-      { credsPlugin = "google"
-      , credsIdent = userId
-      , credsExtra = setExtra token userResponse
-      }
 
 newtype User = User Text
 
@@ -65,25 +23,28 @@ instance FromJSON User where
 
 oauth2GoogleScopedWidget :: YesodAuth m => WidgetFor m () -> [Text] -> Text -> Text -> AuthPlugin m
 oauth2GoogleScopedWidget widget scopes clientId clientSecret =
-    authOAuth2Widget widget pluginName oauth2 $ \manager token -> do
-        (User userId, userResponse) <- authGetProfile
-            pluginName
-            manager
-            token
-            "https://www.googleapis.com/oauth2/v3/userinfo"
+  authOAuth2Widget widget pluginName oauth2 $ \manager token -> do
+    (User userId, userResponse) <-
+      authGetProfile
+        pluginName
+        manager
+        token
+        "https://www.googleapis.com/oauth2/v3/userinfo"
 
-        pure Creds
-            { credsPlugin = pluginName
-            , credsIdent = userId
-            , credsExtra = setExtra token userResponse
-            }
+    pure
+      Creds
+        { credsPlugin = pluginName
+        , credsIdent = userId
+        , credsExtra = setExtra token userResponse
+        }
   where
-    oauth2 = OAuth2
+    oauth2 =
+      OAuth2
         { oauthClientId = clientId
         , oauthClientSecret = Just clientSecret
         , oauthOAuthorizeEndpoint =
             "https://accounts.google.com/o/oauth2/auth"
-                `withQuery` [scopeParam " " scopes]
+              `withQuery` [scopeParam " " scopes]
         , oauthAccessTokenEndpoint =
             "https://www.googleapis.com/oauth2/v3/token"
         , oauthCallback = Nothing
