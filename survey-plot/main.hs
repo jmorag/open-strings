4c4ae10c4c78bad5c71f6b7ef6abe381e1d40655
Joseph Morag <jm@josephmorag.com>
2021-03-26 20:22:05 -0400

    Write survey-plot executable to dump survey responses to csv

---
+++ survey-plot/main.hs
@@ -0,0 +1,90 @@
+import Import.NoFoundation
+
+import Control.Monad.Logger (runStderrLoggingT)
+import qualified Data.Text as T
+import Database.Persist
+import Database.Persist.Postgresql
+import Database.Persist.URL
+import Model
+import qualified Data.ByteString.Lazy as BL
+import Data.Csv
+
+import System.Exit (die)
+
+main =
+  getArgs >>= \case
+    [dbUrl] -> run dbUrl
+    _ -> die "Usage: survey-plot [database-url]"
+
+run dbUrl = do
+  PostgresConf {..} <- fromDatabaseUrl 1 dbUrl
+  runStderrLoggingT $
+    withPostgresqlPool pgConnStr pgPoolSize $
+      \pool -> liftIO $
+        flip runSqlPersistMPool pool $ do
+          responses <- getSurveyResponses
+          let csv = encodeDefaultOrderedByName responses
+          liftIO $ BL.writeFile "survey_responses.csv" csv
+
+data SurveyResponse = SurveyResponse
+  { survey_id :: Int64
+  , years_experience :: Int
+  , violinist_type :: Text -- concat multiple with commas
+  , most_recent_piece :: Text -- human readable
+  , survey_question_id :: Int64
+  , piece :: Text -- human readable
+  , comfort :: Int
+  , expressivity :: Int
+  , idiomatic :: Int
+  , fingering_guess :: Int
+  , other :: Maybe Text
+  }
+  deriving stock (Show, Generic)
+  deriving anyclass (ToNamedRecord, DefaultOrdered)
+
+getSurveyResponses :: (MonadUnliftIO m) => ReaderT SqlBackend m [SurveyResponse]
+getSurveyResponses = do
+  keys <- selectKeysList [] []
+  traverse getSurveyResponse keys
+
+getSurveyResponse ::
+  (MonadUnliftIO m) => Key SurveyFingering -> ReaderT SqlBackend m SurveyResponse
+getSurveyResponse survey_fingering_id = do
+  SurveyFingering {..} <- getJust survey_fingering_id
+  SurveyDemographics {..} <- getJust surveyFingeringSurveyId
+  most_recent_piece <- formatWork surveyDemographicsMost_recent_piece
+  piece <- formatEntry surveyFingeringEntryId
+  pure
+    SurveyResponse
+      { survey_id = fromSqlKey surveyFingeringSurveyId
+      , years_experience = surveyDemographicsYears_experience
+      , violinist_type =
+          T.intercalate
+            ","
+            (foldMap (\t -> [tshow t]) surveyDemographicsViolinist_type)
+      , most_recent_piece
+      , survey_question_id = fromSqlKey survey_fingering_id
+      , piece
+      , comfort = surveyFingeringComfort
+      , expressivity = surveyFingeringExpressivity
+      , idiomatic = surveyFingeringIdiomatic
+      , fingering_guess = surveyFingeringFingering_guess
+      , other = surveyFingeringOther
+      }
+
+formatWork ::
+  (MonadUnliftIO m) => Key Work -> ReaderT SqlBackend m Text
+formatWork work_id = do
+  Work {..} <- getJust work_id
+  Composer {..} <- getJust workComposerId
+  pure $ composerName <> ": " <> workTitle
+
+formatEntry entry_id = do
+  Entry {..} <- getJust entry_id
+  Movement {..} <- getJust entryMovementId
+  title <- formatWork movementWorkId
+  let movement = case movementNumber of
+        0 -> ""
+        n -> tshow n <> ". " <> movementName
+      measures = tshow entryMeasure_start <> "-" <> tshow entryMeasure_end
+  pure $ title <> " " <> movement <> " mm " <> measures
