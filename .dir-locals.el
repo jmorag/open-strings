5c4786b4a4aca1def5b8b21037d8518e6a4d1932
Joseph Morag <jm@josephmorag.com>
2021-09-01 14:49:20 -0500

    Update dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,3 @@
-((hamlet-mode . ((hamlet/basic-offset . 4)
-                 (haskell-process-use-ghci . t))))
+((haskell-mode . ((haskell-process-type . stack-ghci)))
+ (hamlet-mode . ((hamlet/basic-offset . 4)))
+ (js-mode . ((js-indent-level . 2))))
